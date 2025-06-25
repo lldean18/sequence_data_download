2497a2a38a70453200ba01cc73d84e03e2bdd97d
Laura Dean <lldean18@gmail.com>
2025-06-25 10:50:58 +0100

    adding download info for another hagfish assembly and trying with a slightly different link

--- ENA_assembly_download.bash
+++ ENA_assembly_download.bash
@@ -12,18 +12,22 @@
 #SBATCH --time=2:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
-# set variables
-wkdir=/gpfs01/home/mbzlld/data/hagfish/Eptatretus_burgeri_ref # working directory where your information files are stored and where you will download the data to
-accession=GCA_035128595 # the accession number of the assembly you wish to download
+# set working directory where you wish to store downloaded files
+#wkdir=/gpfs01/home/mbzlld/data/hagfish/Eptatretus_burgeri_ref
+wkdir=/gpfs01/home/mbzlld/data/hagfish/Eptatretus_atami_ref
+
 
 # move to the directory in which you want to save the assembly
 cd $wkdir
+
 
 # download the assembly
 # get the link for the fasta assembly file by finding the sequence set on ENA e.g. https://www.ebi.ac.uk/ena/browser/view/FYBX03000000
 # then right click on SET FASTA under Download in the box on the right-hand side of the page and copy the link
 # paste it after wget below
 # e.g. for the Inshore Hagfish Eptatretus burgeri genome assembly:
-wget ftp://ftp.ebi.ac.uk/pub/databases/ena/wgs/public/fyb/FYBX03.fasta.gz
-
+#wget ftp://ftp.ebi.ac.uk/pub/databases/ena/wgs/public/fyb/FYBX03.fasta.gz
+# for the brown hagfish assembly:
+wget https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_035128595.1?download=true&gzip=true
+#wget ftp://ftp.ebi.ac.uk/pub/databases/ena/wgs/public/jax/JAXMNL01.fasta.gz
 
