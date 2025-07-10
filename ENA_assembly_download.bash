abb71a9ee2638913341122f1b035a18d12125266
Laura Dean <lldean18@gmail.com>
2025-07-10 17:11:56 +0100

    adding attempt to download from NCBI

--- ENA_assembly_download.bash
+++ ENA_assembly_download.bash
@@ -15,7 +15,8 @@
 # set working directory where you wish to store downloaded files
 #wkdir=/gpfs01/home/mbzlld/data/hagfish/Eptatretus_burgeri_ref
 #wkdir=/gpfs01/home/mbzlld/data/hagfish/Eptatretus_atami_ref
-wkdir=/gpfs01/home/mbzlld/data/gazelle
+#wkdir=/gpfs01/home/mbzlld/data/gazelle
+wkdir=/gpfs01/home/mbzlld/data/splice_variant_search
 
 # move to the directory in which you want to save the assembly
 cd $wkdir
@@ -26,7 +27,7 @@ cd $wkdir
 #curl -s "https://www.ebi.ac.uk/ena/browser/api/xml/GCA_900186335?download=true" # for the inshore hagfish
 #curl -s "https://www.ebi.ac.uk/ena/browser/api/xml/GCA_035128595?download=true" # for the brown hagfish
 #curl -s "https://www.ebi.ac.uk/ena/browser/api/xml/GCA_019969365.1?download=true" # for Nanger dama
-
+#curl -s "https://www.ebi.ac.uk/ena/browser/api/xml/GCA_000001405.29?download=true" # for Homo sapiens (no URL present for this one)
 
 ### download the assembly
 ### get the url from the curl command above then
@@ -40,9 +41,9 @@ cd $wkdir
 
 ### for the Nanger dama assembly (accession: GCA_019969365.1):
 #wget ftp://ftp.ebi.ac.uk/pub/databases/ena/wgs/public/jah/JAHTZZ01.fasta.gz
-wget ftp://ftp.ebi.ac.uk/pub/databases/ena/wgs/public/cak/CAKJTW01.fasta.gz
+#wget ftp://ftp.ebi.ac.uk/pub/databases/ena/wgs/public/cak/CAKJTW01.fasta.gz
 
-
-
+### for the human assembly (URL copied from the NCBI website
+wget https://api.ncbi.nlm.nih.gov/datasets/v2/genome/accession/GCF_000001405.40/download?include_annotation_type=GENOME_FASTA&include_annotation_type=GENOME_GFF&include_annotation_type=RNA_FASTA&include_annotation_type=CDS_FASTA&include_annotation_type=PROT_FASTA&include_annotation_type=SEQUENCE_REPORT&hydrated=FULLY_HYDRATED
 
 
