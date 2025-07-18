98a3c5f6da3fd1e10ac5011a39a729cc666b3cd3
Laura Dean <lldean18@gmail.com>
2025-07-18 13:35:58 +0100

    tested and working version

--- ENA_raw_pod5_or_fast5_download.bash
+++ ENA_raw_pod5_or_fast5_download.bash
@@ -16,13 +16,14 @@
 wkdir=/gpfs01/home/mbzlld/data # working directory where your information files are stored and where you will download the data to
 accession=ERR12834984 # the run accession number for the file you wish to download
 
+# move to the working dir
+cd $wkdir
 
 ###### get the ftp file location for the file you want to download using the accession number
-cd $wkdir
 curl "https://www.ebi.ac.uk/ena/portal/api/filereport?accession=$accession&result=read_run&fields=submitted_ftp,submitted_format&format=tsv" >> ftp_path.txt
 # modify the tsv file so that it contains only the url
-cut -f2 ftp_path.txt > tmp && mv tmp ftp_path.txt # remove the first column of accessions
-sed -i '1~2d' ftp_path.txt # remove header lines
+cut -f3 ftp_path.txt > tmp && mv tmp ftp_path.txt # remove the first column of accessions
+sed -i '1d' ftp_path.txt # remove header lines
 sed -i 's/;/\n/g' ftp_path.txt # if there are multiple urls on the same line (e.g.) for paired forward and reverse reads split them onto single lines
 
 
