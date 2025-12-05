88cfc9390c8c5af4335fbe7edc5e2bbbebf30de7
Laura Dean <lldean18@gmail.com>
2025-12-05 13:16:54 +0000

    updating script to download zebrafish assembly

--- NCBI_assembly_download.bash
+++ NCBI_assembly_download.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Laura Dean
 # 23/6/25
+# 5/12/25
 # script written for running on the UoN HPC Ada
 
-#SBATCH --job-name=ENA_asm_download
+# script to download a genome assembly from NCBI
+
+#SBATCH --job-name=NCBI_asm_download
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -13,7 +16,8 @@
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 ### set working directory where you wish to store downloaded files
-wkdir=/gpfs01/home/mbzlld/data/splice_variant_search
+#wkdir=/gpfs01/home/mbzlld/data/splice_variant_search
+wkdir=/gpfs01/home/mbzlld/data/danionella
 
 ### move to the directory in which you want to save the assembly
 cd $wkdir
@@ -29,9 +33,17 @@ cd $wkdir
 
 ### download the assembly
 ### for the human assembly (URL copied from the NCBI ftp site)
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.fna.gz
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.fna.gz
 ### for the human annotation (URL copied from the NCBI ftp site)
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.gtf.gz
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.gtf.gz
+
+
+### for the zebrafish
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/049/306/965/GCF_049306965.1_GRCz12tu/GCF_049306965.1_GRCz12tu_genomic.fna.gz
+### and the annotation
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/049/306/965/GCF_049306965.1_GRCz12tu/GCF_049306965.1_GRCz12tu_genomic.gff.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/049/306/965/GCF_049306965.1_GRCz12tu/GCF_049306965.1_GRCz12tu_genomic.gtf.gz
+
 
 
 
