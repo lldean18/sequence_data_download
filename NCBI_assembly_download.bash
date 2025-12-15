1a3377aad261d6da5c9277f586447cf6e8d4c3af
Laura Dean <lldean18@gmail.com>
2025-12-15 14:40:35 +0000

    downolading chinese hamster assembly

--- NCBI_assembly_download.bash
+++ NCBI_assembly_download.bash
@@ -17,7 +17,9 @@
 
 ### set working directory where you wish to store downloaded files
 #wkdir=/gpfs01/home/mbzlld/data/splice_variant_search
-wkdir=/gpfs01/home/mbzlld/data/danionella
+#wkdir=/gpfs01/home/mbzlld/data/danionella
+#wkdir=/gpfs01/home/mbzlld/data/ctenella/symbionts
+wkdir=/gpfs01/home/mbzlld/data/ctenella/hamster
 
 ### move to the directory in which you want to save the assembly
 cd $wkdir
@@ -39,11 +41,22 @@ cd $wkdir
 
 
 ### for the zebrafish
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/049/306/965/GCF_049306965.1_GRCz12tu/GCF_049306965.1_GRCz12tu_genomic.fna.gz
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/049/306/965/GCF_049306965.1_GRCz12tu/GCF_049306965.1_GRCz12tu_genomic.fna.gz
 ### and the annotation
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/049/306/965/GCF_049306965.1_GRCz12tu/GCF_049306965.1_GRCz12tu_genomic.gff.gz
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/049/306/965/GCF_049306965.1_GRCz12tu/GCF_049306965.1_GRCz12tu_genomic.gtf.gz
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/049/306/965/GCF_049306965.1_GRCz12tu/GCF_049306965.1_GRCz12tu_genomic.gff.gz
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/049/306/965/GCF_049306965.1_GRCz12tu/GCF_049306965.1_GRCz12tu_genomic.gtf.gz
 
+
+### for ctenella
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/964/212/085/GCA_964212085.1_pySymPilo4.1/GCA_964212085.1_pySymPilo4.1_genomic.fna.gz
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/001/939/145/GCA_001939145.1_ASM193914v1/GCA_001939145.1_ASM193914v1_genomic.fna.gz
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/009/767/595/GCA_009767595.1_ASM976759v1/GCA_009767595.1_ASM976759v1_genomic.fna.gz
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/905/221/605/GCA_905221605.1_Snat_CCMP2548/GCA_905221605.1_Snat_CCMP2548_genomic.fna.gz
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/905/231/915/GCA_905231915.1_Snec_CCMP2469/GCA_905231915.1_Snec_CCMP2469_genomic.fna.gz
+
+
+### for the hamster
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/668/045/GCF_003668045.3_CriGri-PICRH-1.0/GCF_003668045.3_CriGri-PICRH-1.0_genomic.fna.gz
 
 
 
